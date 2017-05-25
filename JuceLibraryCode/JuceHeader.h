05a3975a05cb4ca46de3593f59616c0f6eb3b652
Willow Rosenberg <wistro@users.noreply.github.com>
2017-05-25 23:01:30 +0100

    nevermind, changed the name back

--- JuceLibraryCode/JuceHeader.h
+++ JuceLibraryCode/JuceHeader.h
@@ -39,7 +39,7 @@
 #if ! JUCE_DONT_DECLARE_PROJECTINFO
 namespace ProjectInfo
 {
-    const char* const  projectName    = "QuidStream Assistant";
+    const char* const  projectName    = "QuidStreamAssistant";
     const char* const  versionString  = "0.0.1";
     const int          versionNumber  = 0x1;
 }
