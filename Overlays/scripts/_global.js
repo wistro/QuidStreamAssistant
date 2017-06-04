85f04f0a58d985b53a05cfd65e87b52188d40861
Willow Rosenberg <wistro@users.noreply.github.com>
2017-06-04 14:22:05 +0100

    switched to atom for overlay dev, netbeans is awful

--- Overlays/scripts/_global.js
+++ Overlays/scripts/_global.js
@@ -1,6 +1,3 @@
-var MINI = require('minified'); 
-var _=MINI._, $=MINI.$, $$=MINI.$$, EE=MINI.EE, HTML=MINI.HTML;
-
 var visible, timeOld, timeNew, updating;
 var parser = new DOMParser();
 var responseXml;
