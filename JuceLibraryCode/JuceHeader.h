e79dfba2af32c45bfa35485274aabcadca898ea2
Willow Rosenberg <wistro@users.noreply.github.com>
2017-06-04 13:29:17 +0100

    starting JS/CSS/HTML dev for overlays

--- JuceLibraryCode/JuceHeader.h
+++ JuceLibraryCode/JuceHeader.h
@@ -40,7 +40,7 @@
 namespace ProjectInfo
 {
     const char* const  projectName    = "QuidStreamAssistant";
-    const char* const  versionString  = "0.1.0";
-    const int          versionNumber  = 0x100;
+    const char* const  versionString  = "0.6.0";
+    const int          versionNumber  = 0x600;
 }
 #endif
