aa5b8e04a4bbe75f382208e0f887b8fb37343b33
Willow Rosenberg <wistro@users.noreply.github.com>
2017-07-06 21:52:22 +0200

    pronouns are much nicer now

--- Source/Gameplay/PronounsAndLogo.cpp
+++ Source/Gameplay/PronounsAndLogo.cpp
@@ -33,8 +33,80 @@ void PronounsAndLogo::paint (Graphics& g)
   Rectangle<int> area (getLocalBounds());
   
   logo.setBounds(area);
-  g.drawFittedText(pronounChart.joinIntoString("   "), area, Justification::centred, (area.getHeight() / g.getCurrentFont().getHeight()) );
-
+  
+  Rectangle<int> left (area.removeFromLeft(proportionOfWidth(0.33f)));
+  Rectangle<int> right (area.removeFromRight(proportionOfWidth(0.33f)));
+  
+  const int rows = area.getHeight() / g.getCurrentFont().getHeight();
+  
+  if ( rows >= pronounChart.size() )
+  {
+    g.drawFittedText(pronounChart.joinIntoString("\n"), area, Justification::centred, rows);
+  }
+  else if ( rows > (pronounChart.size() / 2) )
+  {
+    String l = "";
+    String r = "";
+    int half = pronounChart.size() / 2;
+    
+    for ( int i = 0; i < half; i++ )
+    {
+      l += pronounChart[i] + "\n";
+      r += pronounChart[half + i] + "\n";
+    }
+    
+    if ( pronounChart.size() % 2 )
+    {
+      r += pronounChart[pronounChart.size()  - 1];
+    }
+    
+    g.drawFittedText(l, left, Justification::centredRight, 0);
+    g.drawFittedText(r, right, Justification::centredLeft, 0);
+  }
+  else
+  {
+    String l = "";
+    String m = "";
+    String r = "";
+    int third = pronounChart.size() / 3;
+    
+    switch(pronounChart.size() % 3)
+    {
+      case 0:
+        for ( int i = 0; i < third; i++ )
+        {
+          l += pronounChart[i] + "\n";
+          m += pronounChart[third + i] + "\n";
+          r += pronounChart[(third * 2) + i] + "\n";
+        }
+        break;
+      case 1:
+        for ( int i = 0; i < third; i++ )
+        {
+          l += pronounChart[i] + "\n";
+          m += pronounChart[third + 1 + i] + "\n";
+          r += pronounChart[(third * 2) + 1 + i] + "\n";
+        }
+        
+        l += pronounChart[third];
+        break;
+      case 2:
+        for ( int i = 0; i < third; i++ )
+        {
+          l += pronounChart[i] + "\n";
+          m += pronounChart[third + 1 + i] + "\n";
+          r += pronounChart[(third + 1) * 2 + i] + "\n";
+        }
+        
+        l += pronounChart[third];
+        m += pronounChart[(third * 2) + 1];
+        break;
+    }
+    
+    g.drawFittedText(l, left, Justification::centredRight, 0);
+    g.drawFittedText(m, area, Justification::centred, 0);
+    g.drawFittedText(r, right, Justification::centredLeft, 0);
+  }
 }
 
 void PronounsAndLogo::resized()
